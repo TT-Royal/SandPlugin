23cadb873bfbc97de34abc2a018181e5e3942ae2
TT-Royal <99549949+TT-Royal@users.noreply.github.com>
2022-02-27 22:24:51 +0100

    Delete Plugins/src directory

--- Plugins/src/fr/ttroyal/seaplugin/utils/Utils.java
+++
@@ -1,8 +0,0 @@
-package fr.ttroyal.seaplugin.utils;
-
-import fr.ttroyal.seaplugin.NukkitMain;
-
-public class Utils {
-    public static NukkitMain instance;
-    public static NukkitMain getInstance() { return instance; }
-}
